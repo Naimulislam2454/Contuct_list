92f5449bf3a1e11ad1bc676847958ef9f42f63e8
Naimulislam2454 <naiem11972454@gmail.com>
2021-10-07 12:03:31 -0700

    hello mahbub

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-center">
          <Link to='/' className='navbar-brand ml-5'>Welcome to react readux book contuct</Link>
         
         </nav>
